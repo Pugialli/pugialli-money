e05910a2c12a1e350c26d20965fadd111fd6d21a
Pugialli <joaopugialli@gmail.com>
2024-06-18 09:10:09 -0300

    env update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { ThemeProvider } from "styled-components";
-// import { TransactionsProvider } from "./contexts/TransactionsContext";
 import { Transactions } from "./pages/Transactions";
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
